8964705579ae0339943a0e33419053b6d40fafd5
everton.rodrigues <everton.rodrigues@integrada.coop.br>
2023-12-21 17:59:35 -0300

    primeiro commit teste

---
+++ manipulacao-arquivos/src/main/java/com/jacksonEverton/manipulacaoarquivos/ManipulacaoArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.jacksonEverton.manipulacaoarquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipulacaoArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManipulacaoArquivosApplication.class, args);
+	}
+
+}
---
+++ manipulacao-arquivos/src/test/java/com/jacksonEverton/manipulacaoarquivos/ManipulacaoArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonEverton.manipulacaoarquivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManipulacaoArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
